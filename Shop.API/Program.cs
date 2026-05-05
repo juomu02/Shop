df28e6a7484c2e83152b4c9505f4c41fe17435a0
juozas <muleviciusj@gmail.com>
2026-05-05 14:16:12 +0300

    added notes

--- Shop.API/Program.cs
+++ Shop.API/Program.cs
@@ -7,7 +7,8 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 builder.Services.AddControllers();
 
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
@@ -21,16 +22,10 @@ builder.Services.AddScoped<IUserRepository, UserRepository>();
 builder.Services.AddScoped<IUserService, UserService>();
 
 var app = builder.Build();
-
-// .csproj reikalinga <PackageReference Include="Swashbuckle.AspNetCore.SwaggerUI" Version="10.1.7" /> 
-app.UseSwaggerUI(options => {
-    options.SwaggerEndpoint("/openapi/v1.json", "v1");
-});
-
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
